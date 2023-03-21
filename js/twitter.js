e33e3bbdffa99614820f66accdc887b530db2bb8
TanakaPiyoco <tanaka.piyoco@gmail.com>
2023-03-21 22:32:12 +0900

    色々

--- js/twitter.js
+++
@@ -1,7 +0,0 @@
-$(window).on('load', function() {
-
-  var $widget = $('iframe.twitter-timeline');
-  var $widgetContents = $widget.contents();
-  $widgetContents.find('head').append('<link href="../css/SNS.css" rel="stylesheet" type="text/css">');
-
-});
